95c96a3d32caec93c11a32ddd5c2aa563fedf60c
amerkhanfar <abk.sg8@gmail.com>
2023-12-04 18:00:17 +0400

    added loading and design to confirm page

---
+++ app/data/columns.tsx
@@ -0,0 +1,145 @@
+"use client";
+import axios from "axios";
+import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown, MoreHorizontal } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import updateData, { updatePending } from "./delete";
+import { useRouter } from "next/navigation";
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type Payment = {
+  id: string;
+  email: string;
+  name: string;
+};
+
+const apiKey = process.env.NEXT_PUBLIC_API_KEY?.toString();
+const tagToAdd = "attended";
+
+const axiosConfig = {
+  headers: {
+    Authorization: `Bearer ${apiKey}`,
+  },
+};
+
+const deleteData = async (data: any) => {
+  console.log(data);
+  try {
+    await axios.delete(`/api/lists/12a63504d0/members/${data}`, axiosConfig);
+    console.log("data deleted");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const updateTag = async (data: any) => {
+  console.log(data);
+  try {
+    await axios.post(
+      `/api/lists/12a63504d0/members/${data}/tags`,
+      {
+        tags: [{ name: "attended", status: "active" }],
+      },
+      axiosConfig,
+    );
+    console.log(tagToAdd);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getTag = async (data: any) => {
+  console.log(data);
+  try {
+    await axios.post(
+      `/api/lists/12a63504d0/members/${data}/tags`,
+      {
+        tags: [{ name: "test-dev", status: "active" }],
+      },
+      axiosConfig,
+    );
+    return <div>amer</div>;
+  } catch (error) {
+    return <div>sabbagh</div>;
+    console.log(error);
+  }
+};
+
+export const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: "index",
+    header: "#",
+  },
+  {
+    accessorKey: "name",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='ghost'
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+          Name
+          <ArrowUpDown className='ml-2 h-4 w-4' />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: "email",
+    header: "Email",
+  },
+  {
+    accessorKey: "org",
+    header: "Organization",
+  },
+  {
+    accessorKey: "status",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='ghost'
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+          Status
+          <ArrowUpDown className='ml-2 h-4 w-4' />
+        </Button>
+      );
+    },
+
+    cell: ({ row }) => {
+      const payment = row.original.id;
+
+      return (
+        <div>
+          <Button
+            onClick={async () => {
+              // console.log(row.original.id);
+              await updateTag(row.original.id);
+              location.reload();
+            }}
+            style={{
+              background: "#a7742c",
+              width: "80px",
+              display: "flex",
+              borderRadius: "7px",
+              padding: "5px 0px",
+              justifyContent: "center",
+              color: "white",
+              cursor: "pointer",
+            }}
+            variant='ghost'
+            className='h-8 w-8 p-0'>
+            Accept
+          </Button>
+        </div>
+      );
+    },
+  },
+];
---
+++ app/data/data-table.tsx
@@ -0,0 +1,213 @@
+"use client";
+import * as React from "react";
+import Link from "next/link";
+import { Oval } from "react-loader-spinner";
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  flexRender,
+  SortingState,
+  VisibilityState,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+  loading: boolean;
+}
+
+export function DataTable<TData, TValue>({
+  columns,
+  data,
+  loading,
+}: DataTableProps<TData, TValue>) {
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    [],
+  );
+
+  const [columnVisibility, setColumnVisibility] =
+    React.useState<VisibilityState>({});
+  const table = useReactTable({
+    data,
+    columns,
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    state: {
+      sorting,
+      columnVisibility,
+      columnFilters,
+    },
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <div>
+      <div className='flex flex-col items-center py-4 w-screen justify-center gap-10'>
+        <h1 style={{ fontSize: "20px" }} className='heading'>
+          RSVP Attendees
+        </h1>
+
+        <div
+          style={{
+            position: "relative",
+            width: "90%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}>
+          <div
+            style={{
+              position: "absolute",
+              left: "0",
+              width: "20%",
+              display: "flex",
+              justifyContent: "space-between",
+            }}>
+            <div>Total : {data.length}</div>
+          </div>
+
+          <div className='contains-links'>
+            <Link href={"/attendance"} className='other'>
+              View RSVP Response
+            </Link>
+            <Link href={"/approved"} className='other'>
+              View Approved
+            </Link>
+            <Link href={"/attended"} className='other'>
+              View Attendance
+            </Link>
+          </div>
+        </div>
+
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}>
+          <Input
+            placeholder='Search...'
+            value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
+            onChange={(event) =>
+              table.getColumn("name")?.setFilterValue(event.target.value)
+            }
+            className='max-w-sm'
+          />
+        </div>
+      </div>
+
+      <div className='rounded-md border flex items-center w-screen'>
+        <Table>
+          <TableHeader style={{ background: "#a7742c" }}>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id} style={{ color: "white" }}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext(),
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {loading ? (
+              <TableRow
+                className='text-center'
+                style={{
+                  display: "flex",
+                  justifyContent: "flex-end",
+                  width: "55vw",
+                }}>
+                <TableCell
+                  colSpan={columns.length}
+                  className='h-24 text-center'>
+                  <Oval
+                    height={80}
+                    width={80}
+                    color='#a7742c'
+                    wrapperStyle={{}}
+                    wrapperClass=''
+                    visible={true}
+                    ariaLabel='oval-loading'
+                    secondaryColor='#ebac55'
+                    strokeWidth={2}
+                    strokeWidthSecondary={2}
+                  />
+                </TableCell>
+              </TableRow>
+            ) : (
+              <>
+                {table.getRowModel().rows?.length ? (
+                  table.getRowModel().rows.map((row) => (
+                    <TableRow
+                      style={{ borderBottom: "1px solid rgba(0,0,0,0.2)" }}
+                      key={row.id}
+                      data-state={row.getIsSelected() && "selected"}>
+                      {row.getVisibleCells().map((cell) => (
+                        <TableCell key={cell.id}>
+                          {flexRender(
+                            cell.column.columnDef.cell,
+                            cell.getContext(),
+                          )}
+                        </TableCell>
+                      ))}
+                    </TableRow>
+                  ))
+                ) : (
+                  <TableRow
+                    className='text-center'
+                    style={{
+                      display: "flex",
+                      justifyContent: "flex-end",
+                      width: "55vw",
+                    }}>
+                    <TableCell
+                      colSpan={columns.length}
+                      className='h-24 text-center'>
+                      <p style={{ fontSize: "15px" }}>No Result</p>
+                    </TableCell>
+                  </TableRow>
+                )}
+              </>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
+}
